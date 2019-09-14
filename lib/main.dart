d71017bd9d55215eb189dd91c75472779dd753ff
at1208 <amantiwari12081999@gmail.com>
2019-09-14 17:09:08 +0530

    styling added

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,32 @@ class App extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.cyan[50],
         body: Center(
 
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.indigo,
+                 radius: 50,
+                backgroundColor: Colors.indigo[100],
               ),
-             Text('Aman Tiwari', style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold
+             Padding(padding: EdgeInsets.only(bottom: 20)),
+             Text('Aman Tiwari', style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold, letterSpacing: 4
              ),
              ),
-              Text('Full Stack Web Developer',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400,color: Colors.black
+             Padding(padding: EdgeInsets.only(bottom: 20)),
+              Text('Full Stack Web Developer',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400,color: Colors.black, backgroundColor: Colors.deepPurpleAccent[100]
               
-             ),),
+             ),
+             ),
+               Padding(padding: EdgeInsets.only(bottom: 5)),
              FlatButton(
+              color: Colors.pinkAccent[100],
               onPressed: () => {},
               child: Column(
                 children: <Widget>[
-                  Text('www.amantiwari.me', style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold
+                  Text('www.amantiwari.me', style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold, 
              ),
              ),
                   Text('+919140283263', style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold
