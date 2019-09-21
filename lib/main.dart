b09c5d86087f8c2e3a1e079210022a98459f6e9f
at1208 <amantiwari12081999@gmail.com>
2019-09-21 06:07:31 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -19,28 +19,39 @@ class App extends StatelessWidget{
             children: <Widget>[
               CircleAvatar(
                  radius: 50,
+                 backgroundImage: AssetImage('images/at.png'),
                 backgroundColor: Colors.indigo[100],
               ),
              Padding(padding: EdgeInsets.only(bottom: 20)),
-             Text('Aman Tiwari', style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold, letterSpacing: 4
+             Text('Aman Tiwari', style: TextStyle(fontSize: 37, fontWeight: FontWeight.bold, letterSpacing: 4
              ),
              ),
              Padding(padding: EdgeInsets.only(bottom: 20)),
-              Text('Full Stack Web Developer',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400,color: Colors.black, backgroundColor: Colors.deepPurpleAccent[100]
-              
+             Row(children: <Widget>[
+               Icon(Icons.laptop_mac),
+                Text('Full Stack Web Developer',style: TextStyle(fontSize: 25,fontWeight: FontWeight.w400,color: Colors.black, backgroundColor: Colors.deepPurpleAccent[100]
              ),
-             ),
+             )
+             ],mainAxisAlignment: MainAxisAlignment.center,)
+             ,
                Padding(padding: EdgeInsets.only(bottom: 5)),
              FlatButton(
               color: Colors.pinkAccent[100],
               onPressed: () => {},
               child: Column(
                 children: <Widget>[
-                  Text('www.amantiwari.me', style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold, 
+                  Row(children: <Widget>[
+                  Icon(Icons.cloud_circle),
+                  Text('www.amantiwari.me', style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold, 
              ),
              ),
-                  Text('+919140283263', style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold
+                  ],mainAxisAlignment: MainAxisAlignment.center,),
+                  Row(children: <Widget>[
+                  Icon(Icons.call, color: Colors.green,),
+                  Text('+919140283263', style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold
              ),)
+                  ],mainAxisAlignment: MainAxisAlignment.center,),
+                 
                 ],
               )
              ),
